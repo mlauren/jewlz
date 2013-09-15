87a4eecebe4702353947c06609a0f83182276426
Lauren M <mlmxwell@gmail.com>
2013-09-15 18:24:25 -0500

    This is my first commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Googoogaga::Application.config.secret_key_base = '3dc9ca8b5b530be369bc9791fcefc8fa45228a8376bd393fabc8018c1f30b6a08c7c6d45aa4249ccbea2eb9e244819c2b5ed915306501c5a2f6f211e87aef00e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Googoogaga::Application.config.session_store :cookie_store, key: '_googoogaga_session'
